177e720942cb6bbe1543b41ac08e407048ae9fd5
mperdiguero <perdi666@gmail.com>
2021-05-25 15:13:26 +0200

    as

--- src/pages/HomePage.page.tsx
+++ src/pages/HomePage.page.tsx
@@ -1,12 +1,12 @@
+import React from "react";
 import { CardContainer } from "../pods/card/CardHome.container";
 import { TransactionContainer } from "../pods/transaction/TransactionHome.container";
 
-export const HomePage : React.FC = () => {
-  return(
+export const HomePage: React.FC = () => {
+  return (
     <>
       <CardContainer />
-      <TransactionContainer/>
+      <TransactionContainer />
     </>
-  ) 
+  );
 };
-
