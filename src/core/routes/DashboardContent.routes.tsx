4f43492be91e3e0f381ec5fa6647ce625d967181
mperdiguero <perdi666@gmail.com>
2021-05-27 09:25:40 +0200

    fixed context

--- src/core/routes/DashboardContent.routes.tsx
+++ src/core/routes/DashboardContent.routes.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import { Switch, Route, useRouteMatch } from "react-router-dom";
-import { MyContextProvider } from "../../common-components/context-provider/dashboard.context";
 import AccountsPage from "../../pages/AccountsPage.page";
 import BalancePage from "../../pages/BalancePage.page";
 import { CardsPage } from "../../pages/CardsPage.page";
@@ -15,22 +14,21 @@ export const DashboardContent: React.FC = () => {
   return (
     <div>
       <Switch>
-        <MyContextProvider>
-          <Route path={`${path}${switchRoutes.home}`} component={HomePage} />
-          <Route
-            path={`${path}${switchRoutes.accounts}`}
-            component={AccountsPage}
-          />
-          <Route path={`${path}${switchRoutes.cards}`} component={CardsPage} />
-          <Route
-            path={`${path}${switchRoutes.transactions}`}
-            component={TransactionsPage}
-          />
-          <Route
-            path={`${path}${switchRoutes.balance}`}
-            component={BalancePage}
-          />
-        </MyContextProvider>
+        <Route path={`${path}${switchRoutes.home}`} component={HomePage} />
+
+        <Route
+          path={`${path}${switchRoutes.accounts}`}
+          component={AccountsPage}
+        />
+        <Route path={`${path}${switchRoutes.cards}`} component={CardsPage} />
+        <Route
+          path={`${path}${switchRoutes.transactions}`}
+          component={TransactionsPage}
+        />
+        <Route
+          path={`${path}${switchRoutes.balance}`}
+          component={BalancePage}
+        />
       </Switch>
     </div>
   );
