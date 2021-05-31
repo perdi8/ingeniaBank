b8a1e02456613da21d375d4fe2f341f6aabc4f15
mperdiguero <perdi666@gmail.com>
2021-05-31 16:53:34 +0200

    fixed response error

--- src/services/Account.service.tsx
+++ src/services/Account.service.tsx
@@ -8,13 +8,8 @@ export const GetAccountList = () => {
   const loadAccountList = () => {
     fetch(`https://bethabank.herokuapp.com/api/accounts?id=${id}`)
       .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else if (response.status === 404) {
-          return Promise.reject("error 404");
-        } else {
-          return Promise.reject("some other error: " + response.status);
-        }
+        if (!response.ok) throw Error("Error 404");
+        return response;
       })
       .then((json) => setAccountList(json))
       .catch(function (err) {
