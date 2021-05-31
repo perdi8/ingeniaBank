1d0f601df7b347c2f6966e208a14d2daaafcb4e2
mperdiguero <perdi666@gmail.com>
2021-05-31 16:17:44 +0200

    fixed error fetch account service

--- src/services/Account.service.tsx
+++ src/services/Account.service.tsx
@@ -3,13 +3,31 @@ import { MyContext } from "../common-components/context-provider/dashboard.conte
 
 export const GetAccountList = () => {
   const { id } = React.useContext(MyContext);
-  const [accountList, setAccountList] = useState([]);
+  const [accountList, setAccountList] = useState<any>([]);
 
+  /*
   const loadAccountList = () => {
     fetch(`https://bethabank.herokuapp.com/api/accounts?id=${id}`)
       .then((response) => response.json())
       .then((json) => setAccountList(json))
       .catch((error) => console.error(error));
+  };
+*/
+  const loadAccountList = () => {
+    fetch(`https://bethabank.herokuapp.com/api/accounts?id=${id}`)
+      .then((response) => {
+        if (response.ok) {
+          response.json();
+          return;
+        } else {
+          // eslint-disable-next-line no-throw-literal
+          throw "No existen cuentas";
+        }
+      })
+      .then((json) => setAccountList(json))
+      .catch(function (err) {
+        console.log(err);
+      });
   };
 
   return { loadAccountList, accountList };
