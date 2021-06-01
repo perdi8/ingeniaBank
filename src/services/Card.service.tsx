eb44b2acd47bbd067117659ef7c87b08ae091355
mperdiguero <perdi666@gmail.com>
2021-06-01 08:23:36 +0200

    fixed error card list service

--- src/services/Card.service.tsx
+++ src/services/Card.service.tsx
@@ -4,7 +4,7 @@ import { CardList } from "../models/card/Card.model";
 
 export const GetCardList = () => {
   const { id } = React.useContext(MyContext);
-  const [cardList, setCardList] = useState<CardList>();
+  const [cardList, setCardList] = useState<any>();
   const [responseCardList, setResponseCardList] = useState<any>();
 
   const loadCardList = () => {
