3a96deadecf914ffb465eaf98b340757c3fb500b
mperdiguero <perdi666@gmail.com>
2021-06-01 09:24:03 +0200

    fixed service card

--- src/common-components/card/CardCommon.container.tsx
+++ src/common-components/card/CardCommon.container.tsx
@@ -1,21 +1,15 @@
 import React, { useEffect } from "react";
-import { Card } from "../../models/card/Card.model";
 import { GetCardList } from "../../services/Card.service";
 import { CardCommonComponent } from "./CardCommon.component";
 
 export const CardCommonContainer: React.FC = () => {
   const { loadCardList, cardList } = GetCardList();
-  const [list, setList] = React.useState<any>([]);
+  const [list, setList] = React.useState([]);
 
   useEffect(() => {
     loadCardList();
-    if (cardList !== undefined) {
-      console.log(cardList + "containeeeeeeeeeee");
-      setList(cardList);
-    }
-
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cardList]);
+  }, []);
 
-  return <CardCommonComponent cardList={list} />;
+  return <CardCommonComponent cardList={cardList} />;
 };
--- src/services/Card.service.tsx
+++ src/services/Card.service.tsx
@@ -1,30 +1,24 @@
 import React, { useState } from "react";
 import { MyContext } from "../common-components/context-provider/dashboard.context";
-import { Card, CardList } from "../models/card/Card.model";
 
 export const GetCardList = () => {
   const { id } = React.useContext(MyContext);
-  const [cardList, setCardList] = useState();
-  const [responseCardList, setResponseCardList] = useState<any>([]);
-
-  React.useEffect(() => {
-    if (responseCardList !== undefined) {
-      setCardList(responseCardList.cardList);
-      console.log(responseCardList);
-    }
-  }, [responseCardList]);
+  const [cardList, setCardList] = useState<any>([]);
 
   const loadCardList = () => {
     fetch(`https://bethabank.herokuapp.com/api/cards?id=${id}`)
       .then((response) => {
-        if (!response.ok) throw Error("Error 404");
-        return response;
+        if (response.ok) {
+          return response.json();
+        } else if (response.status === 404) {
+          return Promise.reject("error 404");
+        } else {
+          return Promise.reject("some other error: " + response.status);
+        }
       })
-      .then((json) => {
-        setResponseCardList(json);
-        console.log(json);
-      })
+      .then((json) => setCardList(json))
       .catch((error) => console.error(error));
   };
+
   return { loadCardList, cardList };
 };
